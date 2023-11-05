5a19b4434dab023ea3b11ee608948192c2cf726e
HarrisonHemstreet <HarrisonHemstreet@gmail.com>
2023-11-05 12:05:06 -0700

    cleaned up styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './assets/css/main.css'
+import './styling/css/main.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
